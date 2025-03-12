a33fca140a59c5a06c858bec184648bea93fbcff
Ethan-6626 <wklein1121@outlook.com>
2025-03-12 20:37:45 +0800

    Disk与Unit类的初步实现

--- code/Disk.h
+++ code/Disk.h
@@ -1,8 +1,31 @@
 #pragma once
 #pragma GCC optimize("O2")
 #include "utils.h"
+/**
+
+@brief 磁盘类
+该类用于描述存储系统中的一个磁盘，它管理磁盘上存储单元的信息以及磁头的状态。
+磁盘包含多个存储单元（块），同时维护以下属性：
+@param id 磁盘的唯一标识符
+@param empty_num 当前磁盘中空闲存储单元的数量
+@param point 磁头当前所在的存储单元编号
+@param object_num 磁盘中已存储的对象数量
+@param object_id 当前磁盘中存储了那些对象
+@param point_status 磁头状态，true 表示空闲，false 表示忙碌
+@param rest_tokens 当前时间片中磁头剩余可使用的令牌数
+@param first_empty 第一个空闲存储单元的位置
+@param size 磁盘总的存储单元数
+
+**/
 
 class Disk {
 public:
-
+    int id;
+    int empty_num;
+    int point;
+    int object_num;
+    std::unordered_map<int, bool> object_id;
+    int rest_tokens;
+    int first_empty;
+    int size;
 };
--- code/Unit.h
+++ code/Unit.h
@@ -2,7 +2,33 @@
 #pragma GCC optimize("O2")
 #include "utils.h"
 
+/**
+
+@brief 存储单元（Unit）类
+该类用于表示硬盘中的一个存储单元。一个硬盘可能包含成千上万个存储单元，
+每个存储单元对应硬盘上的一个块。
+示例：
+假设硬盘上有两个对象，其中对象1由3个存储单元构成，对象2由4个存储单元构成，
+则属性示例如下：
+ 对象1(3块)        对象2(4块)
+ ------------------------------
+object_id:   1   2   3     4   5   6   7
+block_order: 1   2   3     1   2   3   4
+@param disk_id 存储单元所在的磁盘编号
+@param block_id 存储单元中存储的对象编号
+@param object_id 对象块的标识ID（单调递增）
+@param id 存储单元在磁盘中的具体位置
+@param is_exist 标志存储单元是否被占用
+
+*/
+
 class Unit {
 public:
+	int disk_id; 
+	int block_id; 
+	int object_id; 
+	int block_order; 
+	int id; 
+	bool is_exist; 
 
 };
